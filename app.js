0bdf4d834c7ab327790d3089fab2f1f9e31b746e
minamihiroto <kokowatamf@gmail.com>
2022-01-10 22:33:05 +0900

    requestが不足していた時のエラーを記述

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 // jsonを使えるように宣言
-app.use(express.json())
+app.use(express.json());
 
-app.post('/booklog', (req,res)=>{
+app.post("/booklog", (req, res) => {
   // requestのbodyをそのまま定数に入れている
-  const booklog = req.body
+  const booklog = req.body;
 
-  res.json({
-      'ok': true,
-      'booklog': booklog
-    })
-})
+  if (booklog.name && booklog.text) {
+    res.json({
+      ok: true,
+      booklog: booklog,
+    });
+  } else {
+    res.json({
+      ok: false,
+      error: 'nameかtextがrequestされていません'
+    });
+  }
+});
 
 // node app.jsで起動
 // listenとは外部からアクセスがあるとそれに対応する処理を行って表示を送り返す関数
-app.listen(port, ()=>{console.log('listenできてるよ')})
+app.listen(port, () => {
+  console.log("listenできてるよ");
+});
