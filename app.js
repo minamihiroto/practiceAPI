4a247ed14ddebf1abf615d86f2465e3f98b892a6
minamihiroto <kokowatamf@gmail.com>
2022-01-10 22:25:16 +0900

    postを叩くとresponseが帰ってくるように記述

--- app.js
+++ app.js
@@ -2,5 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// jsonを使えるように宣言
+app.use(express.json())
+
+app.post('/booklog', (req,res)=>{
+  res.json({
+      'ok': true
+    })
+})
+
 // node app.jsで起動
+// listenとは外部からアクセスがあるとそれに対応する処理を行って表示を送り返す関数
 app.listen(port, ()=>{console.log('listenできてるよ')})
